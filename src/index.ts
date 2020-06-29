be9f0d5be69054a3418b91fc14b7892b7f3bed3c
kurianowiczz <42355857+kurianowiczz@users.noreply.github.com>
2020-06-29 19:04:09 +0300

    changed index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { Worker } from 'worker_threads';
 import router from './router';
 import { firebaseConfig } from './database';
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 export const app = express();
 
 app.use(bodyParser.json());
